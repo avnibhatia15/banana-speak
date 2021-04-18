b82669b3eee8934a7a025cfe3b81d4dcab72e160
Avneet Kaur Bhatia <avnibhatia15@gmail.com>
2021-04-18 17:13:07 +0530

    import javaScript

---
+++ app.js
@@ -0,0 +1,7 @@
+// console.log("Script is working from different file");
+//input
+var userName=prompt("Give me your username");
+//processing 
+var welcomeMessage="This script works!! "+userName;
+//output
+alert(welcomeMessage);
