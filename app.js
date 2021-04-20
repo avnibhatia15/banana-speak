df88af8340def5b06d4fbb5b0d4699480bb4a802
Avneet Kaur Bhatia <avnibhatia15@gmail.com>
2021-04-21 01:27:59 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var btnTranslate = document.querySelector("#btn-translate");
 //querySelector is a way of telling the browser that Hey browser i need that elemrnt which has id #btn-translate
 var txtInput=document.querySelector("#txt-input");
-console.log(txtInput)
+var outputDiv = document.querySelector("#output");
+
+// console.log(txtInput)
 
 function clickEventHandler(){
-    console.log("clicked!");
-    console.log("input",txtInput.value)
+    outputDiv.innerText = "abbcdvifjncklkldkm "+ txtInput.value;
+    // console.log("clicked!");
+    // console.log("input",txtInput.value)
 //Hey browser remember that btnTranslate reference you gave, could you please add a clickListener to it.Browser says "Yes, What do you want me to do when click happens ".Just console.log(). 
 };
 btnTranslate.addEventListener("click",clickEventHandler);
