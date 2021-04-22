27961cba912d5c00f85ea777d8980ef053bc9e66
Avneet Kaur Bhatia <avnibhatia15@gmail.com>
2021-04-22 16:13:25 +0530

    done

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 var btnTranslate = document.querySelector("#btn-translate");
-//querySelector is a way of telling the browser that Hey browser i need that elemrnt which has id #btn-translate
 var txtInput=document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json"
 
-// console.log(txtInput)
+
+function getTranslationURL(input){
+ return serverURL + "?" + "text=" + input
+ alert("Something wrong with the server.Try again after sometime.")}
+
+function errorHandler(error){
+    console.log("error occured",error);
+}
 
 function clickEventHandler(){
-    outputDiv.innerText = "abbcdvifjncklkldkm "+ txtInput.value;
-    // console.log("clicked!");
-    // console.log("input",txtInput.value)
-//Hey browser remember that btnTranslate reference you gave, could you please add a clickListener to it.Browser says "Yes, What do you want me to do when click happens ".Just console.log(). 
+   var inputText = txtInput.value; //taking input
+
+   //calling server for processing
+fetch(getTranslationURL(inputText))
+  .then(response => response.json())
+  .then(json =>{
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+  })
+  .catch(errorHandler)
 };
 btnTranslate.addEventListener("click",clickEventHandler);
+ 
